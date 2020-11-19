fd4106c5e59e507dd388cad4592dfe90442f8091
juhahinkula <hinkula@gmail.com>
2020-11-19 11:16:09 +0200

    first login fixed

--- src/main/java/fi/haagahelia/course/WebSecurityConfig.java
+++ src/main/java/fi/haagahelia/course/WebSecurityConfig.java
@@ -29,7 +29,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
             .formLogin()
                 .loginPage("/login")
-                .defaultSuccessUrl("/students")
+                .defaultSuccessUrl("/students", true)
                 .permitAll()
             	.and()
             .logout()
