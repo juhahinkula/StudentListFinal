e96c570a6481c926c2c902b4a14b328fc154d16a
juhahinkula <hinkula@gmail.com>
2023-08-01 14:35:46 +0300

    Spring Boot 3.1.2 upgrade

--- src/main/java/fi/haagahelia/course/WebSecurityConfig.java
+++ src/main/java/fi/haagahelia/course/WebSecurityConfig.java
@@ -6,22 +6,27 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.servlet.util.matcher.MvcRequestMatcher;
+import org.springframework.web.servlet.handler.HandlerMappingIntrospector;
 
 import fi.haagahelia.course.service.UserDetailServiceImpl;
 
 @Configuration
+@EnableWebSecurity
 @ComponentScan("fi.haagahelia.course")
 public class WebSecurityConfig {
 	@Autowired
 	private UserDetailServiceImpl userDetailsService;
 
 	@Bean
-	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+	public SecurityFilterChain filterChain(HttpSecurity http, HandlerMappingIntrospector introspector) throws Exception {
+		MvcRequestMatcher.Builder mvcMatcherBuilder = new MvcRequestMatcher.Builder(introspector);
 		return http
 			.authorizeHttpRequests(authorizeHttpRequests -> authorizeHttpRequests
-				.requestMatchers("/css/**", "/signup", "/saveuser").permitAll()
+				.requestMatchers(mvcMatcherBuilder.pattern("/css/**"), mvcMatcherBuilder.pattern("/signup"), mvcMatcherBuilder.pattern("/saveuser")).permitAll()
 				.anyRequest().authenticated())
 			.formLogin(form -> form.loginPage("/login").defaultSuccessUrl("/students", true).permitAll())
 			.logout(logout -> logout.permitAll()).build();
